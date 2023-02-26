12b3bc6dfd04eb9eb2aaf2ab047b06d71db5ba07
NishantSingh02 <nst7372@gmail.com>
2023-02-26 23:42:26 +0530

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+
+import NavBar from "./Components/NavBar/NavBar";
+import Home from "./pages/home/Home";
+import LogIn from "./pages/login/LogIn";
+import SignUp from "./pages/signup/SignUp";
+import Settings from "./pages/settings/Settings";
+
+import { useAuthContext } from "./hooks/useAuthContext";
 
 function App() {
+  const { user, authIsReady } = useAuthContext();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {authIsReady && (
+        <Router>
+          <NavBar />
+          <Routes>
+            <Route
+              path="/"
+              element={!user ? <Navigate to="/login" /> : <Home />}
+            />
+            <Route
+              path="/login"
+              element={user ? <Navigate to="/" /> : <LogIn />}
+            />
+            <Route
+              path="/signup"
+              element={user ? <Navigate to="/" /> : <SignUp />}
+            />
+            <Route
+              path="/settings"
+              element={user ? <Settings /> : <Navigate to="/login" />}
+            />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </Router>
+      )}
     </div>
   );
 }
