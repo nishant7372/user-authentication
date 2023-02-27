37e48898dab99d0163f9ed2625406ce0a8d24818
NishantSingh02 <nst7372@gmail.com>
2023-02-27 11:30:31 +0530

    init commit

---
+++ src/context/AuthContext.js
@@ -0,0 +1,58 @@
+import { createContext, useEffect, useReducer } from "react";
+import axios from "axios";
+
+export const AuthContext = createContext();
+
+export const authReducer = (state, action) => {
+  switch (action.type) {
+    case "LOGIN":
+      return { ...state, user: action.payload };
+    case "LOGOUT":
+      return { ...state, user: null };
+    case "AUTH_IS_READY":
+      return { ...state, user: action.payload, authIsReady: true };
+    case "SESSION_INDEX":
+      return { ...state, currentSession: action.payload };
+    default:
+      return state;
+  }
+};
+
+// only on refresh
+const readProfile = async (token, dispatch) => {
+  try {
+    const res = await axios({
+      method: "get",
+      url: "http://localhost:3000/users/me",
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-type": "application/json; charset=UTF-8",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    // dispatch auth_is_ready
+
+    dispatch({ type: "AUTH_IS_READY", payload: res.data });
+  } catch (err) {
+    dispatch({ type: "AUTH_IS_READY", payload: null });
+  }
+};
+
+export const AuthContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(authReducer, {
+    user: null,
+    authIsReady: false,
+    currentSession: null,
+  });
+
+  useEffect(() => {
+    readProfile(localStorage.getItem("token"), dispatch);
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ ...state, dispatch }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
