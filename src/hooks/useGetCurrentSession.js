0fff53164cec8213769acdc82e1e0fe9ec5e35a4
NishantSingh02 <nst7372@gmail.com>
2023-02-27 11:31:06 +0530

    init commit

---
+++ src/hooks/useGetCurrentSession.js
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import axios from "axios";
+import { useAuthContext } from "./useAuthContext";
+
+export const useGetCurrentSession = () => {
+  const [error, setError] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+  const { dispatch } = useAuthContext();
+
+  const getCurrentSession = async () => {
+    setError(null);
+    setIsPending(true);
+    const header = localStorage.getItem("token");
+    try {
+      const res = await axios({
+        method: "get",
+        url: "http://localhost:3000/users/currentSession",
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Content-type": "application/json; charset=UTF-8",
+          Authorization: `Bearer ${header}`,
+        },
+      });
+      setIsPending(false);
+      setError(null);
+      dispatch({
+        type: "SESSION_INDEX",
+        payload: res.data.currentSessionIndex,
+      });
+      return res;
+    } catch (err) {
+      setError(err);
+      setIsPending(false);
+      dispatch({ type: "SESSION_INDEX", payload: null });
+    }
+  };
+  return { getCurrentSession, error, isPending };
+};
