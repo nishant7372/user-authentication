20430b0b55d264b4443e0374c5d31a01934d97a6
NishantSingh02 <nst7372@gmail.com>
2023-02-28 10:37:49 +0530

    init commit

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,15 +1,9 @@
-import { useAuthContext } from "../../hooks/useAuthContext";
 import "./Home.css";
 
 export default function Home() {
-  const { user } = useAuthContext();
   return (
     <div className="home">
-      Home page
-      <div>
-        <div>{user.name}</div>
-        <div>{user.age}</div>
-      </div>
+      <div className="center">Home Page</div>
     </div>
   );
 }
